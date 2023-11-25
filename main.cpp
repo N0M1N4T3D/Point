9790411b6df74b84a6fe7f858d07f863a6fa3f86
mefikan <lev.evgenyev@yandex.ru>
2023-11-25 12:21:17 +0300

    Задание 1

--- main.cpp
+++ main.cpp
@@ -2,43 +2,57 @@
 
 class Point2D{
 public:
+    Point2D(){}
+    Point2D(int a, int b) : x(a), y(b){}
     int set_x(int x){
         this->x = x;
     }
-
     int set_y(int y){
         this->y = y;
     }
-
     void get_x(){
         std::cout << "x: " << this->x << "\n";
     }
-
     void get_y(){
         std::cout << "y: " << this->y << "\n";
     }
-
-private:
+protected:
     int x,y;
 };
 
 class Z{
 public:
+    Z(){}
+    Z(int a) : z(a) {}
     int set_z(int z){
         this->z = z;
     }
-
     void get_z(){
         std::cout << "z: " << this->z << "\n";
     }
-private:
+protected:
     int z;
 };
 
-class Point3D: Point2D, Z {
-
+class Point3D: public Point2D, public Z {
+public:
+    Point3D(int a, int b, int c)
+    {
+        this->x = a;
+        this->y = b;
+        this->z = c;
+    }
+    void get_info()
+    {
+        std::cout << "x: " << x << " y: " << y << " z: " << z << '\n';
+    }
 };
 
 int main() {
+    Point3D p1(10,42,52), p2(10,10,10), p3(4,1,5),p4(0,9,3);
+    p1.get_info();
+    p2.get_info();
+    p3.get_info();
+    p4.get_info();
     return 0;
 }
